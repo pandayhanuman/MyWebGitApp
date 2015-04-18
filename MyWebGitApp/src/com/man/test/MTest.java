f9c042f04d11aed3a6b65a0da2f262f5a86b74d6
hanuman <panday.hanuman@gmail.com>
2015-04-18 20:19:55 +0530

    modified by hanuman

--- MyWebGitApp/src/com/man/test/MTest.java
+++ MyWebGitApp/src/com/man/test/MTest.java
@@ -1,9 +1,10 @@
 package com.man.test;
 
-/*this is a general comment......*/
+/*this is a general comment......1323232*/
 public class MTest {
 
 	public static void main(String[] args) {
+		System.out.println("MTest.main()");
 		System.out.println("hanuman");
 
 	}
